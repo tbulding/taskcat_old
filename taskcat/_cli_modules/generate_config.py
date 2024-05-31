696b9b62c403de6c0ac61c6f9ec26f4010313228
Tony Bulding <tbulding@amazon.com>
2024-05-30 19:44:13 -0500

    fixed issue when template is present and replace is selected

--- taskcat/_cli_modules/generate_config.py
+++ taskcat/_cli_modules/generate_config.py
@@ -1,8 +1,9 @@
 import logging
 from pathlib import Path
 
-from taskcat.project_config.config import ConfigGenerator
 from taskcat.exceptions import TaskCatException
+from taskcat.project_config.config import ConfigGenerator
+
 LOG = logging.getLogger(__name__)
 
 
@@ -21,14 +22,19 @@ class GenerateConfig:
         user_email: str = "noreply@acme.com",
         project_root: str = "./",
         aws_region: str = "us-east-1",
-        replace: bool = False
+        replace: bool = False,
     ):
 
         project_root_path = Path(project_root).expanduser().resolve()
         if not project_root_path.exists():
-            raise TaskCatException(f"Project root path {project_root_path} does not exist")
-        ConfigGenerator(main_template=main_template,
-                        output_file=output_file,
-                        project_root_path=str(project_root_path),
-                        owner_email=user_email,
-                        aws_region=aws_region, replace=replace).generate_config()
+            raise TaskCatException(
+                f"Project root path {project_root_path} does not exist"
+            )
+        ConfigGenerator(
+            main_template=main_template,
+            output_file=output_file,
+            project_root_path=str(project_root_path),
+            owner_email=user_email,
+            aws_region=aws_region,
+            replace=replace,
+        ).generate_config()
--- taskcat/project_config/config.py
+++ taskcat/project_config/config.py
@@ -1,6 +1,6 @@
 import logging
-import sys
 import os
+import sys
 from pathlib import Path
 
 import git
@@ -19,16 +19,21 @@ LOG_CONFIG.setLevel(logging.ERROR)
 
 class ConfigGenerator:
     def __init__(
-            self, main_template: str, output_file: str,
-            project_root_path: str, owner_email: str,
-            aws_region: str, replace: bool):
+        self,
+        main_template: str,
+        output_file: str,
+        project_root_path: str,
+        owner_email: str,
+        aws_region: str,
+        replace: bool,
+    ):
         self.output_file = output_file
         self.main_template = main_template
         self.project_root_path = project_root_path
         self.owner_email = owner_email
         self.aws_region = aws_region
         self.repo = git.Repo(project_root_path, search_parent_directories=True)
-        self.repo_name = self.repo.remotes.origin.url.split('.git')[0].split('/')[-1]
+        self.repo_name = self.repo.remotes.origin.url.split(".git")[0].split("/")[-1]
         self.replace = replace
 
     def generate_config(self):
@@ -43,51 +48,52 @@ class ConfigGenerator:
         if not template_path.exists():
             raise TaskCatException(f"Template file {template_path} does not exist")
 
-
         LOG.warning("This is an ALPHA feature. Use with caution")
         # Read Yaml file
-        with open(template_path, 'r', encoding="utf-8") as f:
+        with open(template_path, "r", encoding="utf-8") as f:
             cfn = yaml.load(f)
         f.close()
         # Container for each parameter object
         parameters = {}
         # Get data for each parameter
-        for n in cfn['Parameters']:
-            parameters[n] = cfn['Parameters'][n]
+        for n in cfn["Parameters"]:
+            parameters[n] = cfn["Parameters"][n]
 
         # Create a dict for the config content
-        cfg_dict = OrderedDict({
-            "project": OrderedDict({
-                "name": self.repo_name,
-                "owner": self.owner_email,
-                "regions": [self.aws_region],
-                "parameters": _add_parameter_values(parameters)
-            }),
-            "tests": OrderedDict({
-                "default": OrderedDict({
-                    "template": self.main_template
-                })
-            })
-        })
+        cfg_dict = OrderedDict(
+            {
+                "project": OrderedDict(
+                    {
+                        "name": self.repo_name,
+                        "owner": self.owner_email,
+                        "regions": [self.aws_region],
+                        "parameters": _add_parameter_values(parameters),
+                    }
+                ),
+                "tests": OrderedDict(
+                    {"default": OrderedDict({"template": self.main_template})}
+                ),
+            }
+        )
 
         # Config File Handling
         config_file = Path(project_config_path)
         if config_file.exists():
             # Check if file is empty
             if os.stat(project_config_path).st_size == 0:
-                LOG.warning("Empty project config found. "
-                            "Overwriting...")
+                LOG.warning("Empty project config found. " "Overwriting...")
                 os.remove(project_config_path)
             elif os.stat(project_config_path).st_size > 0 and not self.replace:
-                raise TaskCatException("Project config file already exists. "
-                                       "No changes will be made. "
-                                       "to override use --replace flag")
-                # LOG.warning("Project config file already exists. "
-                #             "No changes will be made. to override use --replace flag")
-                # sys.exit(1)
+                raise TaskCatException(
+                    "Project config file already exists. "
+                    "No changes will be made. "
+                    "to override use --replace flag"
+                )
             else:
-                LOG.warning("Project config file already exists. "
-                            "Overwriting existing file")
+                LOG.warning(
+                    "Project config file already exists. " "Overwriting existing file"
+                )
+                os.remove(project_config_path)
         try:
             config = Config.create(
                 args=cfg_dict,
@@ -97,17 +103,19 @@ class ConfigGenerator:
                 env_vars={
                     "TASKCAT_PROJECT_PACKAGE_LAMBDA": "True",
                     "TASKCAT_PROJECT_SHORTEN_STACK_NAME": "True",
-                    "TASKCAT_PROJECT_S3_REGIONAL_BUCKETS": "True"},
+                    "TASKCAT_PROJECT_S3_REGIONAL_BUCKETS": "True",
+                },
             )
             LOG.info("Config file created successfully")
         except TaskCatException as e:
             LOG.error(e)
             sys.exit(1)
 
-        with open(f'{self.project_root_path}/{self.output_file}',
-                  "w+", encoding="utf-8") as outfile:
+        with open(
+            f"{self.project_root_path}/{self.output_file}", "w+", encoding="utf-8"
+        ) as outfile:
             config_dict = config.config.to_dict()
-            config_dict.pop('general')
+            config_dict.pop("general")
             yaml.dump(config_dict, outfile)
         outfile.close()
-        LOG.info(_get_parameter_stats(cfg_dict['project']['parameters']))
+        LOG.info(_get_parameter_stats(cfg_dict["project"]["parameters"]))
